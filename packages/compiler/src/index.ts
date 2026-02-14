989be6cfda8a9c53cfaaa78e4d8cd79b12188335
kalakish <kalariya.kishan123@gmail.com>
2026-02-14 09:18:55 +0000

    1402

--- packages/compiler/src/index.ts
+++ packages/compiler/src/index.ts
@@ -1,16 +1,6 @@
-export * from './compiler';
-export * from './parser/lexer';
-export * from './parser/parser';
-export * from './parser/ast';
-export * from './validator/symbol-table';
-export * from './validator/type-checker';
-export * from './validator/validator';
-export * from './generator/metadata-generator';
-export * from './generator/sql-generator';
-export * from './generator/typescript-generator';
-export * from './generator/schema-generator';
-export * from './optimizer/query-optimizer';
-export * from './optimizer/code-optimizer';
-
-import { NovaCompiler } from './compiler';
-export default NovaCompiler;
+export class ALCompiler {
+  compile(source: string): any {
+    console.log("Compiling AL source...");
+    return { success: true };
+  }
+}
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,39 +1,5 @@
-// Core Application
-export * from './application';
-export * from './session';
-export * from './company';
-export * from './tenant';
-export * from './workflow';
-export * from './extension';
-
-// Database
-export * from '../database/connection';
-export * from '../database/transaction';
-export * from '../database/query-builder';
-
-// Events
-export * from '../events/dispatcher';
-export * from '../events/subscriber';
-export * from '../events/queue';
-
-// Data Types
-export * from '../data-types/code';
-export * from '../data-types/decimal';
-export * from '../data-types/date';
-export * from '../data-types/datetime';
-export * from '../data-types/option';
-export * from '../data-types/blob';
-export * from '../data-types/mediaset';
-
-// Factories
-export * from './factories/session-factory';
-export * from './factories/record-factory';
-export * from './factories/page-factory';
-
-// Utils
-export * from './utils/logger';
-export * from './utils/helpers';
-export * from './utils/validators';
-
-import { NovaApplication } from './application';
-export default NovaApplication;
+export class NovaCore {
+  initialize() {
+    console.log("NOVA Core initialized");
+  }
+}
